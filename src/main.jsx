db1bb5f1dcdfbc4cd8e6baa333edaeca837b3636
Elvita <elvitamhraniii24@gmail.com>
2026-07-07 11:34:12 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Preloader from './components/Preloader.jsx';
@@ -9,28 +9,21 @@ import './index.css';
 const Main = () => {
   const [isLoading, setIsLoading] = useState(true);
 
-  const handlePreloaderFinished = () => {
-    setIsLoading(false);
-  };
-
   return (
-    <>
+    <ThemeProvider>
       {isLoading ? (
-        <Preloader onFinished={handlePreloaderFinished} />
+        <Preloader onFinished={() => setIsLoading(false)} />
       ) : (
-        <App />
+        <HashRouter>
+          <App />
+        </HashRouter>
       )}
-    </>
+    </ThemeProvider>
   );
 };
 
-// Render komponen Main ke dalam DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <Main />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <Main />
+  </React.StrictMode>
 );
